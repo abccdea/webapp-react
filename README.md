05ae7f59818c93de7acccbe4cdccfc875db4fb27
Zuowen Wang <wang.zuowen@me.com>
2019-01-03 16:48:33 +0800

    react 路由

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-## 安装
+## 安装环境
 
 ```
 yarn add webpack webpack-dev-server babel-core babel-loader babel-preset-es2015 babel-preset-stage-0 style-loader css-loader less-loader file-loader url-loader html-webpack-plugin babel-preset-react --save-dev
 ```
-
+## 安装react
+```
+yarn add react react-dom redux react-redux react-router-dom redux-logger redux-thunk redux-promise axios
+```
 ## 入口文件
 src/index.js
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('hello world');
+import React from 'react';
+import ReactDom, { render } from 'react-dom';
+import Home from './containers/Home/Home';
+import Lesson from './containers/Lesson/Lesson';
+import Profile from './containers/Profile/Profile';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+render(<HashRouter>
+    <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/lesson" component={Lesson} />
+        <Route path="/profile" component={Profile} />
+    </Switch>
+</HashRouter>, document.getElementById('root'))
